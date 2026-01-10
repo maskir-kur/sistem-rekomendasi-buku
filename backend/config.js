2426ffb0dcbb61fa49955f42e3b7a48d7a13bb74
maskir-kur <maskirkurkur2@gmail.com>
2026-01-10 23:59:32 +0700

    ADD: url db eror

--- backend/config.js
+++ backend/config.js
@@ -1,10 +1,7 @@
-import dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
 
 const config = {
-  db: {
-    databaseUrl: process.env.DATABASE_URL,
-  },
+  databaseUrl: process.env.DATABASE_URL,
   jwtSecret: process.env.JWT_SECRET,
 };
 
--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,10 @@
 import mysql from "mysql2/promise";
 import config from "./config.js";
 
+if (!config.databaseUrl) {
+  throw new Error("DATABASE_URL is missing");
+}
+
 const pool = mysql.createPool(config.databaseUrl);
 
 export default pool;
