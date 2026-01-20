10a828a62f73461bb0a925bf75d0cdc460103016
maskir-kur <maskirkurkur2@gmail.com>
2026-01-20 22:20:55 +0700

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -16,22 +16,22 @@ const app = express();
  * ✅ CORS CONFIG (WAJIB PALING ATAS)
  * =====================================
  */
-const cors = require("cors");
+const allowedOrigins = [
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://sistem-rekomendasi-buku.vercel.app");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+];
 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
+app.use(cors({
+  origin: 'https://sistem-rekomendasi-buku.vercel.app',            
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
-app.use(cors());
+app.options("*", cors());
 
+
+// WAJIB untuk preflight request
+app.options("*", cors());
 
 /**
  * =====================================
